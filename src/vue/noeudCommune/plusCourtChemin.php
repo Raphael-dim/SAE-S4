3865e9ad90b3589a5557f8c6d27747b71609cc96
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2023-03-18 18:40:23 +0100

    map

--- src/vue/noeudCommune/plusCourtChemin.php
+++ src/vue/noeudCommune/plusCourtChemin.php
@@ -1,26 +1,24 @@
 <form class="saisieVille" action="calculer" method="post">
-        <div id="autocompletionDepartDiv">
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="nomCommuneDepart_id">Nom de la commune de départ</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : Menton" name="nomCommuneDepart" id="nomCommuneDepart_id" required>
-                <!-- <img id="loading" src="img/loading.gif"> -->
-            <div class="autocompletion hidden" id="autocompletionDepart"></div>
-            </p>
-        </div>
-        <div id="autocompletionArriveDiv">
-            <p class="InputAddOn">
-                <label class="InputAddOn-item" for="nomCommuneArrivee_id">Nom de la commune d'arrivée</label>
-                <input class="InputAddOn-field" type="text" value="" placeholder="Ex : Menton" name="nomCommuneArrivee" id="nomCommuneArrivee_id" required>
-            </p>
+    <div id="autocompletionDepartDiv">
+        <p class="InputAddOn">
+            <input placeholder="Nom de la commune de départ" class="InputAddOn-field" type="text" value="" placeholder="Ex : Menton" name="nomCommuneDepart" id="nomCommuneDepart_id" required>
             <!-- <img id="loading" src="img/loading.gif"> -->
-            <div class="autocompletion hidden" id="autocompletionArrivee"></div>
-        </div>
-        <div>
-            <input type="hidden" name="XDEBUG_TRIGGER">
-            <p>
-                <input class="InputAddOn-field" type="submit" value="Calculer" />
-            </p>
-        </div>
+        <div class="autocompletion hidden" id="autocompletionDepart"></div>
+        </p>
+    </div>
+    <div id="autocompletionArriveDiv">
+        <p class="InputAddOn">
+            <input placeholder="Nom de la commune d'arrivée" class="InputAddOn-field" type="text" value="" placeholder="Ex : Menton" name="nomCommuneArrivee" id="nomCommuneArrivee_id" required>
+        </p>
+        <!-- <img id="loading" src="img/loading.gif"> -->
+        <div class="autocompletion hidden" id="autocompletionArrivee"></div>
+    </div>
+    <div>
+        <input type="hidden" name="XDEBUG_TRIGGER">
+        <p>
+            <input class="InputAddOn-field" type="submit" value="Calculer" />
+        </p>
+    </div>
 </form>
 
 <?php if (!empty($_POST)) { ?>
