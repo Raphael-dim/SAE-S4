9f5b9f8c7c68cd5f365503f72313181b8cc756bc
Ynox-Proto <yanismakloufi@gmail.com>
2023-03-22 16:41:57 +0100

    dijsktra sql (ancien algo remis en commentaire)

--- src/Modele/DataObject/NoeudRoutier.php
+++ src/Modele/DataObject/NoeudRoutier.php
@@ -34,10 +34,10 @@ class NoeudRoutier extends AbstractDataObject
 
     public function getVoisins(): array
     {
-        if(!isset($this->voisins)){
+        /*if(!isset($this->voisins)){
             $this->voisins = (new NoeudRoutierRepository())->getVoisins($this->getGid());
         }
-        return $this->voisins;
+        return $this->voisins;*/
     }
 
     public function getLongNoeud():string{
