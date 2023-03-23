47dfee83335b1b94fe2c8568578f838e48554dc3
DIMECK Raphael <raphael.dimeck@etu.umontpellier.fr>
2023-03-23 16:40:34 +0100

    fix

--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,6 @@
 const map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8,
+    center: {lat: 46.227638, lng: 2.213749},
+    zoom: 6,
 });
 
 let markerDepart = null;
